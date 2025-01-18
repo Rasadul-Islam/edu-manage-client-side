a3c69ae989160c0071caac829486c0aee14ee3a8
Rasadul Islam <rasadulislam.cse@gmail.com>
2025-01-18 21:57:23 +0600

    added private route

--- src/Routes/Route.jsx
+++ src/Routes/Route.jsx
@@ -1,6 +1,6 @@
 import {
-    createBrowserRouter,
-  } from "react-router-dom";
+  createBrowserRouter,
+} from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../pages/Home/Home";
 import AllClasses from "../pages/AllClasses/AllClasses";
@@ -8,39 +8,45 @@ import TeachOnEdu from "../pages/TeachOnEdu/TeachOnEdu";
 import Register from "../pages/Register/Register";
 import LogIn from "../pages/LogIn/LogIn";
 import Dashboard from "../pages/Dashboard/Dashboard";
+import PrivateRoute from "./PrivateRoute";
 
-  export const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Main></Main>,
-      errorElement:<h1>Here Have some 404 not found Error</h1>,
-      children:[
-        {
-            path:"/",
-            element:<Home></Home>,
-        },
-        {
-            path:"/allClasses",
-            element:<AllClasses></AllClasses>,
-        },
-        {
-            path:"/teachOnEdu",
-            element:<TeachOnEdu></TeachOnEdu>,
-        },
-        {
-            path:"/logIn",
-            element:<LogIn></LogIn>,
-        },
-        {
-            path:"/register",
-            element:<Register></Register>,
-        },
-        {
-            path:"/dashboard",
-            element:<Dashboard></Dashboard>,
-        }
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    errorElement: <h1>Here Have some 404 not found Error</h1>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/allClasses",
+        element: <AllClasses></AllClasses>,
+      },
+      {
+        path: "/teachOnEdu",
+        element:
+          <PrivateRoute>
+            <TeachOnEdu></TeachOnEdu>
+          </PrivateRoute>,
+      },
+      {
+        path: "/logIn",
+        element: <LogIn></LogIn>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
+      },
+      {
+        path: "/dashboard",
+        element:
+          <PrivateRoute>
+            <Dashboard></Dashboard>
+          </PrivateRoute>,
+      }
 
-      ]
-    },
-  ]);
-  
+    ]
+  },
+]);
