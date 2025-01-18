01a0b04364f7680e7b871eb5676b16225b6b4364
Rasadul Islam <rasadulislam.cse@gmail.com>
2025-01-18 22:05:22 +0600

    added Loading Spinner

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,13 +1,18 @@
 import { useContext } from 'react';
 import { AuthContext } from '../providers/AuthProvider';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
+import LoadingSpinner from '../utility/LoadingSpinner/LoadingSpinner';
 
-const PrivateRoute = ({children}) => {
-    const {user} =useContext(AuthContext);
-    if(user){
-        return children;
-    }
-    return <Navigate to='/logIn'></Navigate>
+
+
+const PrivateRoute = ({ children }) => {
+    const { user, loading } = useContext(AuthContext);
+
+    const location = useLocation();
+
+    if (loading) return <LoadingSpinner></LoadingSpinner>
+    if (user) return children;
+    return <Navigate to='/logIn' state={location.pathname}></Navigate>
 };
 
 export default PrivateRoute;
