c6db7e082891fb2a6f58991bc056159de2db90d4
Rasadul Islam <rasadulislam.cse@gmail.com>
2025-01-20 23:19:13 +0600

    set request interceptor in secure api

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -5,6 +5,15 @@ export const axiosSecure =axios.create({
 })
 
 const useAxiosSecure =()=>{
+    axiosSecure.interceptors.request.use(function(config){
+        const token = localStorage.getItem('access-token')
+        console.log('request stop by interceptors', token);
+        config.headers.athorization = `Bearer ${token}`
+        return config;
+    }, function(error){
+        // Do something with request error
+        return Promise.reject(error); 
+    })
     return axiosSecure;
 }
 
